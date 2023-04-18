e229742cc0a9ae4224358661d1c7433081a6adb8
Shivani21-hude <shivani21hude@gmail.com>
2023-04-18 16:51:57 +0530

    [Shivani]-Add new colums and update the values

--- CustomerInfo.sql
+++ CustomerInfo.sql
@@ -17,3 +17,16 @@ insert into Customertable values
 ('Prajakta',9482104732,'Pune')
 
 
+-- add columns in table
+alter table CustomerTable add Country varchar(200),
+Salary money,
+Pincode int
+
+-- update values
+update CustomerTable set Country='Canada' where Customer_Name='Shivani'
+update CustomerTable set Country='India' where Customer_ID in (2,3,4)
+update CustomerTable set Salary=50000 where Customer_ID in (1,3)
+update CustomerTable set Salary=30000 where Customer_ID in(2,4)
+update CustomerTable set Pincode=892341 where Customer_Name='Rohit'
+
+
