8fa72b81e83918957a5bfee4f4d7ecdee2b1320f
Shivani21-hude <shivani21hude@gmail.com>
2023-04-18 16:21:09 +0530

    [Shivani]-Insert values into CustomerTable

--- CustomerInfo.sql
+++ CustomerInfo.sql
@@ -8,3 +8,12 @@ Customer_Name varchar(200),
 PhoneNumber bigint,
 Address varchar(200)
 );
+
+--insert values
+insert into Customertable values
+('Shivani',8456203491,'Pune'),
+('Rohit',9301295824,'Mumbai'),
+('Shubham',8604832194,'Nagpur'),
+('Prajakta',9482104732,'Pune')
+
+
