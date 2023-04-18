f5711fd7c990c94f477c18a4aa9a46faa5cccc80
Shivani21-hude <shivani21hude@gmail.com>
2023-04-18 09:43:08 +0530

    [Shivani]-Create a table name as Customertable

--- CustomerInfo.sql
+++ CustomerInfo.sql
@@ -1,4 +1,10 @@
 ﻿ create database CustomerInfo
 use CustomerInfo
 
-
+-- create customer table  
+create table CustomerTable(
+Customer_ID int identity(1,1) primary key,
+Customer_Name varchar(200),
+PhoneNumber bigint,
+Address varchar(200)
+);
